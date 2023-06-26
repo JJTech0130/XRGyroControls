6d562589846dcd8051a7fefb4a18a25421a8a134
JJTech0130 <jjtech@jjtech.dev>
2023-06-26 14:42:06 -0400

    cleanup writing data and add schema to launch simulator

--- IndigoHID.swift
+++ IndigoHID.swift
@@ -13,7 +13,7 @@ class IndigoHIDMessage {
     public var data: [UInt8] = []
     
     public func as_struct() -> UnsafeMutablePointer<IndigoHIDMessageStruct> {
-        print("data: \(data)")
+        //print("data: \(data)")
         // Make sure that the backing data is the correct size
         guard data.count == MemoryLayout<IndigoHIDMessageStruct>.size else {
             fatalError("IndigoHIDMessage backing data is not the correct size")
@@ -26,38 +26,35 @@ class IndigoHIDMessage {
     
     }
     
+    private func write(_ of: Any, at offset: Int) {
+        let bytes: [UInt8] = withUnsafeBytes(of: of) { Array($0) }
+        data[offset..<offset+bytes.count] = bytes[...]
+    }
+    
     init() {
         // Initialize the backing data to be the correct size, and zeroed out
         data = [UInt8](repeating: 0, count: MemoryLayout<IndigoHIDMessageStruct>.size)
         
         // Set the timestamp to the current time
-        let timestamp = mach_absolute_time()
-        let timestamp_bytes = withUnsafeBytes(of: timestamp) { Array($0) }
-        // Write it at offset 0x24, it's 64 bits
-        data[0x24..<0x24+8] = timestamp_bytes[0..<8]
+        write(mach_absolute_time, at: 0x24)
         
         // Write the constant values
         data[0x18] = 0xA0
         data[0x1C] = 0x01
         data[0x20] = 0x01
         
-        data[0x30..<0x30+4] = [0x2C, 0x01, 0x00, 0x00] // Int32: 300
-    }
-    
-    private func write_float(_ float: Float, offset: Int) {
-        let bytes: [UInt8] = withUnsafeBytes(of: float) { Array($0) }
-        data[offset..<offset+bytes.count] = bytes[...]
+        write(300, at: 0x30)
     }
     
     public func pose(x: Float, y: Float, z: Float, pitch: Float, yaw: Float, roll: Float) {
-        write_float(x, offset: 0x54)
-        write_float(y, offset: 0x58)
-        write_float(z, offset: 0x5C)
+        write(x, at: 0x54)
+        write(y, at: 0x58)
+        write(z, at: 0x5C)
         
-        write_float(pitch, offset: 0x64)
-        write_float(yaw, offset: 0x68)
-        write_float(roll, offset: 0x6C)
+        write(pitch, at: 0x64)
+        write(yaw, at: 0x68)
+        write(roll, at: 0x6C)
         
-        write_float(1.0, offset: 0x70) // Not sure why, but this is important
+        write(1.0, at: 0x70) // Not sure why, but this is important
     }
 }
