b7e6d6386358d34e036b2687a50ec6342259d0ef
JJTech0130 <jjtech@jjtech.dev>
2023-06-26 22:35:14 -0400

    latest findings, still unsure about stuff

--- IndigoHID.swift
+++ IndigoHID.swift
@@ -13,7 +13,6 @@ class IndigoHIDMessage {
     public var data: [UInt8] = []
     
     public func as_struct() -> UnsafeMutablePointer<IndigoHIDMessageStruct> {
-        //print("data: \(data)")
         // Make sure that the backing data is the correct size
         guard data.count == MemoryLayout<IndigoHIDMessageStruct>.size else {
             fatalError("IndigoHIDMessage backing data is not the correct size")
@@ -27,9 +26,7 @@ class IndigoHIDMessage {
     }
     
     private func write<T>(_ of: T, at offset: Int) {
-        //let of = of as! UInt64
         let bytes: [UInt8] = withUnsafeBytes(of: of) { Array($0) }
-        print("Converted \(of) to \(bytes.count) bytes")
         data[offset..<offset+bytes.count] = bytes[...]
     }
     
@@ -44,8 +41,6 @@ class IndigoHIDMessage {
         data[0x18] = 0xA0
         data[0x1C] = 0x01
         data[0x20] = 0x01
-        
-        //write(300, at: 0x30)
     }
     
     public static func camera(x: Float, y: Float, z: Float, pitch: Float, yaw: Float, roll: Float) -> IndigoHIDMessage {
@@ -67,45 +62,56 @@ class IndigoHIDMessage {
     }
     
     public static func manipulator(_ f1: Float, _ f2: Float, _ f3: Float) -> IndigoHIDMessage {
+        // HIDMessage - sending .manipulator with pinching (L=false, R=false), touching (L=false, R=false), matrix: simd_float4x4([[1.0, 0.0, 0.0, 0.0], [0.0, 1.0, 0.0, 0.0], [0.0, 0.0, 1.0, 0.0], [0.0, 0.0, 0.0, 1.0]]), gaze=RERay(origin: SIMD3<Float>(0.0, 0.0, 0.0), direction: SIMD3<Float>(-0.03173566, -0.04234394, -0.99859893), length: 10.0)
+        
         let message = IndigoHIDMessage()
         
         message.write(302, at: 0x30)
         
-        // Constant values
+        // TODO: Expose pinching, figure out all the different possibilities (what is 'touching' for?)
         message.data[0x34] = 3
         
-        message.data[0x38] = 0 // arg4?
+        message.data[0x38] = 0 // ?
         message.data[0x39] = 2
         
-        message.data[0x3A] = 0 // arg5?
+        message.data[0x3A] = 0 // ?
         message.data[0x3B] = 3
         
-        message.data[0x3F] = 0 // arg7
+        message.data[0x3F] = 0 // pinch right
         message.data[0x40] = 1
-        message.data[0x41] = 0 // arg8
+        message.data[0x41] = 0 // ?
         
-        let dfloats = [-0.25, -0.2, -0.3, 1.0]
+        // (I assume) gaze origin
+        message.write(0.0, at: 0x47)
+        message.write(0.0, at: 0x4B)
+        message.write(0.0, at: 0x4F)
         
-        // Pq1
+        // gaze direction
         message.write(f1, at: 0x57)
         message.write(f2, at: 0x5B)
         message.write(f3, at: 0x5F)
         
-        // q0
-        message.write(dfloats[0], at: 0x67)
-        message.write(dfloats[1], at: 0x6B)
-        message.write(dfloats[2], at: 0x6F)
+        // Pose matrix? Maybe related to the pan/yaw/roll thing because it's printing out the same thing as the pre-converted simd matrix
+        // If it is that I have no idea why tf it is duplicated... idk
+        message.write(-0.25, at: 0x67)
+        message.write(-0.2,  at: 0x6B)
+        message.write(-0.3,  at: 0x6F)
+        message.write(0.0,   at: 0x73)
         
-        // q1
-        message.write(dfloats[3], at: 0x83)
+        message.write(0.0,   at: 0x77)
+        message.write(0.0,   at: 0x7B)
+        message.write(0.0,   at: 0x7F)
+        message.write(1.0,   at: 0x83)
         
-        // q2
-        message.write(dfloats[0], at: 0x87)
-        message.write(dfloats[1], at: 0x8B)
-        message.write(dfloats[2], at: 0x8F)
+        message.write(-0.25, at: 0x87)
+        message.write(-0.2,  at: 0x8B)
+        message.write(-0.3,  at: 0x8F)
+        message.write(0.0,   at: 0x93)
         
-        // q3
-        message.write(dfloats[3], at: 0xA3)
+        message.write(0.0,   at: 0x97)
+        message.write(0.0,   at: 0x9B)
+        message.write(0.0,   at: 0x9F)
+        message.write(1.0,   at: 0xA3)
         
         
         return message
--- SimulatorSupport.swift
+++ SimulatorSupport.swift
@@ -15,27 +15,54 @@ import AppKit
         print("XRGyroControls: Initialized HID client")
         super.init()
         
-        var cnt = 0
-        // Schedule a HID message to be sent every 5 seconds
-        Timer.scheduledTimer(withTimeInterval: 5, repeats: true) { timer in
-            cnt += 1
-            self.send_test_message(cnt)
-        }
-    
+        // Reset the camera to the center
+        hid_client.send(message: IndigoHIDMessage.camera(x: 0.0, y: 0.0, z: 0.0, pitch: 0.0, yaw: 0.0, roll: 0.0).as_struct())
     }
     
-    func send_test_message(_ cnt: Int) {
-        print("Sending HID message")
-        //let message = IndigoHIDMessage.pose(x: 0.0, y: Float(cnt) / 1000, z: 0.0, pitch: 0.0, yaw: 0.0, roll: 0.0)
-        // Should create a very slow rise
-        //message.pose(x: 0.0, y: Float(cnt) / 1000, z: 0.0, pitch: 0.0, yaw: 0.0, roll: 0.0)
-        //hid_client.send(message: IndigoHIDMessage.camera(x: 0.0, y: Float(cnt) / 1000, z: 0.0, pitch: 0.0, yaw: 0.0, roll: 0.0).as_struct())
-        hid_client.send(message: IndigoHIDMessage.camera(x: 0.0, y: 0.0, z: 0.0, pitch: 0.0, yaw: 0.0, roll: 0.0).as_struct())
-        hid_client.send(message: IndigoHIDMessage.manipulator(-0.31972468, -0.041431412, -0.9466042).as_struct())
+    var sliders: [NSSlider] = []
+    
+    @objc public func sliderChanged(sender: NSSlider) {
+        //print("Slider changed to \(sender.doubleValue)")
+        hid_client.send(message: IndigoHIDMessage.manipulator(sliders[0].floatValue, sliders[1].floatValue, sliders[2].floatValue).as_struct())
     }
     
     @objc func overlayView() -> NSView {
-        return NSView()
+        let view = NSView()
+        
+        sliders = [NSSlider(target: self, action: #selector(self.sliderChanged)),
+                   NSSlider(target: self, action: #selector(self.sliderChanged)),
+                   NSSlider(target: self, action: #selector(self.sliderChanged))]
+        
+        for slider in sliders {
+            slider.minValue = -1.0
+            slider.maxValue = 1.0
+        }
+        
+        // OK so I think this is a "3D direction vector" of where the eye tracking is pointing
+        // I don't understand 3D maths at all so...
+        sliders[0].floatValue = -0.40874016
+        sliders[1].floatValue = -0.07154943
+        sliders[2].floatValue = -0.9098418
+        
+        
+        // Create a stack of sliders
+        let stack = NSStackView(views: sliders)
+        view.addSubview(stack)
+
+        stack.orientation = .vertical
+        stack.distribution = .fillEqually
+        stack.edgeInsets = NSEdgeInsetsMake(10, 10, 10, 10)
+        
+        let constraints = [
+            stack.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stack.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            stack.widthAnchor.constraint(equalToConstant: 300),
+            stack.heightAnchor.constraint(equalToConstant: 100),
+            stack.heightAnchor.constraint(equalTo: stack.widthAnchor)
+        ]
+        NSLayoutConstraint.activate(constraints)
+
+        return view
     }
     
     @objc func toolbar() -> NSToolbar {
