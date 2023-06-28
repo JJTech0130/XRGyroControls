9eda107692675c5efc2655cc594fad670b9d34a1
JJTech0130 <jjtech@jjtech.dev>
2023-06-28 16:20:25 -0400

    use Spatial framework's abstractions

--- IndigoHID.swift
+++ IndigoHID.swift
@@ -1,12 +1,26 @@
-//
-//  IndigoHID.swift
-//  XRGyroControls
-//
-//  Created by James Gill on 6/25/23.
-//
-
 import Foundation
 import CoreSimulator
+import simd
+import Spatial
+
+// These are probably really inefficient, but whatever they make everything else a ton easier
+extension simd_quatf {
+    // Convert simd_quatd to simd_quatf
+    init(_ quat: simd_quatd) {
+        self.init(ix: Float(quat.imag.x), iy: Float(quat.imag.y), iz: Float(quat.imag.z), r: Float(quat.real))
+        print("Converting \(quat) to \(self)")
+    }
+}
+
+extension simd_float4x4 {
+    // Convert double4x4 to float4x4
+    init(_ mat: double4x4) {
+        self.init(columns: (simd_float4(Float(mat.columns.0.x), Float(mat.columns.0.y), Float(mat.columns.0.z), Float(mat.columns.0.w)),
+                            simd_float4(Float(mat.columns.1.x), Float(mat.columns.1.y), Float(mat.columns.1.z), Float(mat.columns.1.w)),
+                            simd_float4(Float(mat.columns.2.x), Float(mat.columns.2.y), Float(mat.columns.2.z), Float(mat.columns.2.w)),
+                            simd_float4(Float(mat.columns.3.x), Float(mat.columns.3.y), Float(mat.columns.3.z), Float(mat.columns.3.w))))
+    }
+}
 
 class IndigoHIDMessage {
     /// You must NOT change the length to anything other than 0xC0
@@ -27,6 +41,7 @@ class IndigoHIDMessage {
     
     private func write<T>(_ of: T, at offset: Int) {
         let bytes: [UInt8] = withUnsafeBytes(of: of) { Array($0) }
+        print("Writing \(bytes) to offset \(offset)")
         data[offset..<offset+bytes.count] = bytes[...]
     }
     
@@ -43,27 +58,18 @@ class IndigoHIDMessage {
         data[0x20] = 0x01
     }
     
-    public static func camera(x: Float, y: Float, z: Float, pitch: Float, yaw: Float, roll: Float) -> IndigoHIDMessage {
+    public static func camera(_ pose: Pose3D) -> IndigoHIDMessage {
         let message = IndigoHIDMessage()
         
-        message.write(300, at: 0x30)
+        message.write(300 as Int32, at: 0x30)
         
-        message.write(x, at: 0x54)
-        message.write(y, at: 0x58)
-        message.write(z, at: 0x5C)
-        
-        message.write(pitch, at: 0x64)
-        message.write(yaw, at: 0x68)
-        message.write(roll, at: 0x6C)
-        
-        message.write(1.0, at: 0x70) // Not sure why, but this is important
+        message.write(simd_float3(pose.position.vector), at: 0x54)
+        message.write(simd_quatf(pose.rotation.quaternion), at: 0x64)
         
         return message
     }
     
-    public static func manipulator(_ f1: Float, _ f2: Float, _ f3: Float) -> IndigoHIDMessage {
-        // HIDMessage - sending .manipulator with pinching (L=false, R=false), touching (L=false, R=false), matrix: simd_float4x4([[1.0, 0.0, 0.0, 0.0], [0.0, 1.0, 0.0, 0.0], [0.0, 0.0, 1.0, 0.0], [0.0, 0.0, 0.0, 1.0]]), gaze=RERay(origin: SIMD3<Float>(0.0, 0.0, 0.0), direction: SIMD3<Float>(-0.03173566, -0.04234394, -0.99859893), length: 10.0)
-        
+    public static func manipulator(pose: Pose3D, gaze: Ray3D) -> IndigoHIDMessage {
         let message = IndigoHIDMessage()
         
         message.write(302, at: 0x30)
@@ -81,38 +87,10 @@ class IndigoHIDMessage {
         message.data[0x40] = 1
         message.data[0x41] = 0 // ?
         
-        // (I assume) gaze origin
-        message.write(0.0, at: 0x47)
-        message.write(0.0, at: 0x4B)
-        message.write(0.0, at: 0x4F)
+        message.write(simd_float3(gaze.origin.vector), at: 0x47)
+        message.write(simd_float3(gaze.direction.vector), at: 0x57)
         
-        // gaze direction
-        message.write(f1, at: 0x57)
-        message.write(f2, at: 0x5B)
-        message.write(f3, at: 0x5F)
-        
-        // Pose matrix? Maybe related to the pan/yaw/roll thing because it's printing out the same thing as the pre-converted simd matrix
-        // If it is that I have no idea why tf it is duplicated... idk
-        message.write(-0.25, at: 0x67)
-        message.write(-0.2,  at: 0x6B)
-        message.write(-0.3,  at: 0x6F)
-        message.write(0.0,   at: 0x73)
-        
-        message.write(0.0,   at: 0x77)
-        message.write(0.0,   at: 0x7B)
-        message.write(0.0,   at: 0x7F)
-        message.write(1.0,   at: 0x83)
-        
-        message.write(-0.25, at: 0x87)
-        message.write(-0.2,  at: 0x8B)
-        message.write(-0.3,  at: 0x8F)
-        message.write(0.0,   at: 0x93)
-        
-        message.write(0.0,   at: 0x97)
-        message.write(0.0,   at: 0x9B)
-        message.write(0.0,   at: 0x9F)
-        message.write(1.0,   at: 0xA3)
-        
+        message.write(simd_float4x4(pose.matrix), at: 0x67)
         
         return message
     }
--- SimulatorSupport.swift
+++ SimulatorSupport.swift
@@ -2,67 +2,39 @@ import Foundation
 import SimulatorKit
 import CoreSimulator
 import AppKit
+import Spatial
 
 @objc class SimulatorSupport : NSObject, SimDeviceUserInterfacePlugin {
     
     private let device: SimDevice
     private let hid_client: SimDeviceLegacyHIDClient
+    private let server: UDPServer
         
     @objc init(with device: SimDevice) {
         self.device = device
         print("XRGyroControls: Initialized with device: \(device)")
         self.hid_client = try! SimDeviceLegacyHIDClient(device: device)
         print("XRGyroControls: Initialized HID client")
+        
+        //server = try! UDPServer(on: 9985)
+
         super.init()
         
-        // Reset the camera to the center
-        hid_client.send(message: IndigoHIDMessage.camera(x: 0.0, y: 0.0, z: 0.0, pitch: 0.0, yaw: 0.0, roll: 0.0).as_struct())
-    }
-    
-    var sliders: [NSSlider] = []
-    
-    @objc public func sliderChanged(sender: NSSlider) {
-        //print("Slider changed to \(sender.doubleValue)")
-        hid_client.send(message: IndigoHIDMessage.manipulator(sliders[0].floatValue, sliders[1].floatValue, sliders[2].floatValue).as_struct())
+        // Here's an example using a 4x4 transform matrix, as logged by the simulator
+        // Should point the pointer at the "Environments" menu item
+        let fxf = simd_float4x4([[0.9999595, 0.0, -0.008998766, 0.0], [0.0, 1.0, 0.0, 0.0], [0.008998766, 0.0, 0.9999595, 0.0], [0.009089867, 0.0, 0.009959124, 1.0]])
+        
+        let transform = ProjectiveTransform3D(fxf)
+        
+        let pose = Pose3D(transform: transform)!
+        
+        hid_client.send(message: IndigoHIDMessage.camera(pose).as_struct())
+        
+        hid_client.send(message: IndigoHIDMessage.manipulator(pose: pose, gaze: Ray3D(direction: Vector3D(x: -0.40874016, y: -0.07154943, z: -0.9098418))).as_struct())
     }
     
     @objc func overlayView() -> NSView {
-        let view = NSView()
-        
-        sliders = [NSSlider(target: self, action: #selector(self.sliderChanged)),
-                   NSSlider(target: self, action: #selector(self.sliderChanged)),
-                   NSSlider(target: self, action: #selector(self.sliderChanged))]
-        
-        for slider in sliders {
-            slider.minValue = -1.0
-            slider.maxValue = 1.0
-        }
-        
-        // OK so I think this is a "3D direction vector" of where the eye tracking is pointing
-        // I don't understand 3D maths at all so...
-        sliders[0].floatValue = -0.40874016
-        sliders[1].floatValue = -0.07154943
-        sliders[2].floatValue = -0.9098418
-        
-        
-        // Create a stack of sliders
-        let stack = NSStackView(views: sliders)
-        view.addSubview(stack)
-
-        stack.orientation = .vertical
-        stack.distribution = .fillEqually
-        stack.edgeInsets = NSEdgeInsetsMake(10, 10, 10, 10)
-        
-        let constraints = [
-            stack.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            stack.bottomAnchor.constraint(equalTo: view.bottomAnchor),
-            stack.widthAnchor.constraint(equalToConstant: 300),
-            stack.heightAnchor.constraint(equalToConstant: 100),
-            stack.heightAnchor.constraint(equalTo: stack.widthAnchor)
-        ]
-        NSLayoutConstraint.activate(constraints)
-
-        return view
+        return NSView()
     }
     
     @objc func toolbar() -> NSToolbar {
