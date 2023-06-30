2c9968cf7ef00ee36a6dbfdc15f55d2ab144a431
JJTech0130 <jjtech@jjtech.dev>
2023-06-29 22:11:46 -0400

    restruction handlers, add JSON parser (with python test)

--- UDPServer.swift
+++ UDPServer.swift
@@ -41,73 +41,102 @@ class UDPServer {
     func recieveMessage(_ connection: NWConnection) {
         connection.receiveMessage { content, contentContext, isComplete, error in
             if let content = content {
-                print("Recieved message: \(content.base64EncodedString())")
-                var decoded_yet = false
-                // We accept 3 forms of input:
-                // 1. "Position:x,y,z;Rotation:x,y,z,w"
-                // 2. x,y,z,roll,pitch,yaw packed as doubles
-                // 3. JSON message
-                
-                // Check what type it is
-                if content.count == 48 && decoded_yet == false {
-                    print("Trying to decode as OpenTrack data")
-                    // THIS IS NOT TESTED (I'm not at home, don't have a Windows machine
-                    // Convert it to an array of doubles
-                    let doubles = content.withUnsafeBytes { $0.load(as: [Double].self) }
-                    print("Got doubles: \(doubles)")
-                    if doubles.count == 6 {
-                        let point = Point3D(x: doubles[0], y: doubles[1], z: doubles[2])
-                        let euler_angles = EulerAngles(angles: simd_double3(doubles[3], doubles[4], doubles[5]), order: .pitchYawRoll)
-                        let pose = Pose3D(position: point, rotation: Rotation3D(eulerAngles: euler_angles))
-                        let message = IndigoHIDMessage.camera(pose)
-                        self.hid_client.send(message: message.as_struct())
-                        decoded_yet = true
+                for messageHandler in self.MESSAGE_HANDLERS {
+                    // We need to call the handler with ourselves
+                    if (messageHandler(self)(content)) {
+                        // The message was handled, so we can stop
+                        break
                     }
                 }
                 
-                // Try decoding it as a string
-                if decoded_yet == false, let string = String(data: content, encoding: .utf8) {
-                    // Check if the first character is a {
-                    if decoded_yet == false, string.hasPrefix("{") {
-                        print("This looks like JSON")
-                        // TODO: Implement JSON decoding
-                    
-                    }
-                    
-                    if decoded_yet == false, string.hasPrefix("Position") {
-                        // Also didn't test this!!!
-                        print("This looks like @keithahern's format")
-                        let components = string.components(separatedBy: ";")
-                        
-                        let positionString = components[0].replacingOccurrences(of: "Position:", with: "")
-                        let rotationString = components[1].replacingOccurrences(of: "Rotation:", with: "")
-                        
-                        let positionComponents = positionString.components(separatedBy: ",")
-                        let rotationComponents = rotationString.components(separatedBy: ",")
-                        
-                        if positionComponents.count == 3, rotationComponents.count == 4 {
-                            let position = (Float(positionComponents[0]) ?? 0.0, Float(positionComponents[1]) ?? 0.0, Float(positionComponents[2]) ?? 0.0)
-                            let rotation = (Float(rotationComponents[0]) ?? 0.0, Float(rotationComponents[1]) ?? 0.0, Float(rotationComponents[2]) ?? 0.0, Float(rotationComponents[3]) ?? 0.0)
-                            
-                            print("Received position: \(position) and rotation: \(rotation)")
-                            
-                            let point = Point3D(x: position.0, y: position.1, z: position.2)
-                            
-                            let rot = Rotation3D(simd_quatf(ix: rotation.0, iy: rotation.1, iz: rotation.2, r: rotation.3))
-                            
-                            self.hid_client.send(message: IndigoHIDMessage.camera(Pose3D(position: point, rotation: rot)).as_struct())
-                            
-                            decoded_yet = true
-                        }
-                       
-                    }
-                }
-                
-                if decoded_yet == false {
-                    print("Unable to decode message!")
-                }
                 self.recieveMessage(connection)
             }
         }
+    }
+    
+    // Define the message handlers that can handle incoming messages
+    // Return true if the message was handled, false otherwise
+    // Don't force unwrap, if a message is malformed just return false
+    let MESSAGE_HANDLERS = [keithHandler, openTrackHandler, jsonHandler]
+    
+    func jsonHandler(_ data: Data) -> Bool {
+        struct jsonMessage: Codable {
+            struct jsonPose: Codable {
+                var position: simd_float3
+                var rotation: simd_float4
+            }
+            
+            struct jsonGaze: Codable {
+                var origin: simd_float3
+                var direction: simd_float3
+            }
+            
+            struct jsonManipulator: Codable {
+                var pose: jsonPose
+                var gaze: jsonGaze
+            }
+            
+            var camera: jsonPose
+            var manipulator: jsonManipulator
+        }
+        
+        guard let message = try? JSONDecoder().decode(jsonMessage.self, from: data) else { return false }
+        print("Got JSON message: \(message)")
+        
+        self.hid_client.send(message: IndigoHIDMessage.camera(Pose3D(position: message.camera.position, rotation: simd_quatf(vector: message.camera.rotation))).as_struct())
+        
+        self.hid_client.send(message: IndigoHIDMessage.manipulator(
+            pose: Pose3D(position: message.manipulator.pose.position, rotation: simd_quatf(vector: message.manipulator.pose.rotation)),
+            gaze: Ray3D(origin: Point3D(vector: simd_double3(message.manipulator.gaze.origin)), direction: Vector3D(vector: simd_double3(message.manipulator.gaze.direction)))).as_struct())
+        
+        return true
+    }
+    
+    func keithHandler(_ data: Data) -> Bool {
+        guard let string = String(data: data, encoding: .utf8) else { return false }
+        guard string.hasPrefix("Position") else { return false }
+        
+        let components = string.components(separatedBy: ";")
+        
+        let positionString = components[0].replacingOccurrences(of: "Position:", with: "")
+        let rotationString = components[1].replacingOccurrences(of: "Rotation:", with: "")
+        
+        let positionComponents = positionString.components(separatedBy: ",")
+        let rotationComponents = rotationString.components(separatedBy: ",")
+        
+        guard positionComponents.count == 3, rotationComponents.count == 4 else { return false }
+        
+        
+        let position = (Float(positionComponents[0]) ?? 0.0, Float(positionComponents[1]) ?? 0.0, Float(positionComponents[2]) ?? 0.0)
+        let rotation = (Float(rotationComponents[0]) ?? 0.0, Float(rotationComponents[1]) ?? 0.0, Float(rotationComponents[2]) ?? 0.0, Float(rotationComponents[3]) ?? 0.0)
+        
+        print("Received position: \(position) and rotation: \(rotation)")
+        
+        let point = Point3D(x: position.0, y: position.1, z: position.2)
+        
+        let rot = Rotation3D(simd_quatf(ix: rotation.0, iy: rotation.1, iz: rotation.2, r: rotation.3))
+        
+        self.hid_client.send(message: IndigoHIDMessage.camera(Pose3D(position: point, rotation: rot)).as_struct())
+        return true
+    }
+
+    func openTrackHandler(_ data: Data) -> Bool {
+        print("Trying to decode as OpenTrack data")
+        guard data.count == 48 else {
+            return false
+        }
+        // Convert it to an array of doubles
+        let doubles = data.withUnsafeBytes { $0.load(as: [Double].self) }
+        print("Got doubles: \(doubles)")
+        guard doubles.count == 6 else {
+            return false
+        }
+        let point = Point3D(x: doubles[0], y: doubles[1], z: doubles[2])
+        let euler_angles = EulerAngles(angles: simd_double3(doubles[3], doubles[4], doubles[5]), order: .pitchYawRoll)
+        let pose = Pose3D(position: point, rotation: Rotation3D(eulerAngles: euler_angles))
+        let message = IndigoHIDMessage.camera(pose)
+        
+        self.hid_client.send(message: message.as_struct())
+        return true
     }
 }
